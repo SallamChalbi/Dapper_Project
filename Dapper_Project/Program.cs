a495d243f34d8785995eb574cd989caa9381b376
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-17 17:38:54 +0100

    Install Packages (Dapper and Microsoft.Data.SqlClient) and Prepparing the Project

--- Dapper_Project/Program.cs
+++ Dapper_Project/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Dapper_Project
+﻿using Microsoft.Data.SqlClient;
+
+namespace Dapper_Project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            SqlConnection connection = new SqlConnection("Server = .; Database = Northwind; Trusted_Connection = True; TrustedServerCertificate = True");
         }
     }
 }
